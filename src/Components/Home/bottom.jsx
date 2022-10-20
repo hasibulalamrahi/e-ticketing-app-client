82826a67e16e5acc6c9781a7a18771fa4b92e03c
hasibulalamrahi <hasibul.alam@brainstation-23.com>
2022-10-21 05:09:32 +0600

    home page done

---
+++ src/Components/Home/bottom.jsx
@@ -0,0 +1,10 @@
+import "../../Styles/HomePage/bottom.css";
+const BottomSection = () => {
+  return (
+    <div>
+     
+    </div>
+  );
+};
+
+export default BottomSection;
--- src/Components/Home/main.jsx
+++ src/Components/Home/main.jsx
@@ -1,6 +1,13 @@
+import TopSection from './top'
+import TicketSection from './ticket'
+import BottomSection from "./bottom";
 function HomeComponent() {
   return <>
-  
+    <div>
+        <TopSection/>
+        <TicketSection/>
+        <BottomSection/>
+    </div>
   </>;
 };
 
