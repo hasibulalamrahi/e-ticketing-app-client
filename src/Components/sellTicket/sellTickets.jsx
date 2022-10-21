d0780cf0d4a6ac30677d2dc03935ba94a60193e5
hasibulalamrahi <hasibul.alam@brainstation-23.com>
2022-10-21 06:28:12 +0600

    sell ticket route folder structure aligned

---
+++ src/Components/sellTicket/sellTickets.jsx
@@ -0,0 +1,10 @@
+import '../../Styles/SellTicket'
+const SellTicketComp = () => {
+  return (
+    <>
+      <div>Hello From sell ticket page</div>
+    </>
+  );
+};
+
+export default SellTicketComp;
